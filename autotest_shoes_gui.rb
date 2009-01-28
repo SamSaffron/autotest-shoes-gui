e9a82c06ad97eb0df1b7e21b4b0b268aeb9069c1
sam <sam.saffron@gmail.com>
2009-01-28 15:08:34 +1100

    Cleaned up UI and added a few buttons

--- autotest_shoes_gui.rb
+++ autotest_shoes_gui.rb
@@ -84,26 +84,33 @@ else
 # Here is the shoes app ... 
 require 'drb'
 
-Shoes.app do 
+Shoes.app :height => 450, :width => 500, :resizable => true, :title => "autotest" do 
 
-  def update_stats(tests)
+  def update_stats(stack, tests)
+    
+    stack.clear
 
     unless tests     
-      return stack do 
-        tagline strong("Peace on earth - all tests passed!"), :stroke => green
-      end
+        stack.append do 
+          tagline strong("Peace on earth\nAll tests passed!"), 
+            :stroke => green, :left => 150, :top => 120
+        end 
+        return
     end
 
-    stack do 
+    stack.append do 
       tests_have = (tests.count==1 ? "test has":"tests have") 
-      tagline strong("#{tests.count} #{tests_have} failed!"), :stroke => "#ae4116"
+      stack do
+        background linen 
+        tagline strong("#{tests.count} #{tests_have} failed!"), :stroke => "#ae4116", :left => 120
+      end
       prev_file = nil 
       tests.each do |test|
 
         if prev_file != test.file 
           para test.file, 
             " ", 
-            link(">>", :click => Proc.new { system "gvim #{@@rails_root}/#{test.file}" })  
+            link(">>", :click => Proc.new { system "gvim #{@rails_root}/#{test.file}" })  
         end 
         prev_file = test.file
 
@@ -118,20 +125,34 @@ Shoes.app do
       end
     end
   end 
+   
+  @top_stack = stack :height => 400, :scroll => true, :margin_top => 5
 
   drb = DRbObject.new(nil, 'druby://127.0.0.1:98371')
   prev_date = nil 
   s = nil 
   every(1) do 
     if prev_date != drb.last_change
-      @@rails_root ||= drb.rails_root.to_s
+      @rails_root ||= drb.rails_root.to_s
       prev_date = drb.last_change
       tests = nil 
       tests = Array.new(drb.failed_tests) if drb.failed_tests
-      s.clear if s 
-      s = update_stats(tests)
+      update_stats(@top_stack, tests)
     end
   end
+  
+  @bottom_stack = stack :height => 45 do
+    background "#DDD"
+    flow do 
+      button "Restart Site", :margin => 10 do 
+        system "touch #{@rails_root}/tmp/restart.txt"
+      end
+      button "Refresh Schema", :margin => 10 do 
+        system "cd #{@rails_root} && rake db:backup:redo"
+      end
+    end 
+  end
+
 end
 
 
